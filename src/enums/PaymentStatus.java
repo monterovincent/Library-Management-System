cfdf702f1acabdae9ab90f8b79df5f1ec1fd62c6
AdrianGorny8205 <adrian.gorny@edu.sait.ca>
2026-04-20 22:27:19 -0600

    Update PaymentStatus.java

--- src/enums/PaymentStatus.java
+++ src/enums/PaymentStatus.java
@@ -1,5 +1,16 @@
 package enums;
 
+/**
+ * PaymentStatus defines the states of a fine payment
+ * transaction made by a library member.
+ *
+ * Used by:
+ *   - Payment.java    as the status field type
+ *   - FineDAO.java    for filtering and updates
+ */
 public enum PaymentStatus {
-
+    PENDING,
+    COMPLETED,
+    FAILED,
+    REFUNDED
 }
