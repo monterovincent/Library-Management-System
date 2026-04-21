e676e3f5e9f7c7732b32288294be511d26631ccd
AdrianGorny8205 <adrian.gorny@edu.sait.ca>
2026-04-20 22:27:45 -0600

    Update ReservationStatus.java

--- src/enums/ReservationStatus.java
+++ src/enums/ReservationStatus.java
@@ -1,5 +1,17 @@
 package enums;
 
+/**
+ * ReservationStatus defines the lifecycle states
+ * of a book reservation placed by a member.
+ *
+ * Used by:
+ *   - Reservation.java      as the status field type
+ *   - ReservationDAO.java   for filtering and updates
+ *   - ReservationMenu.java  for CLI display
+ */
 public enum ReservationStatus {
-
+    ACTIVE,
+    CANCELLED,
+    FULFILLED,
+    EXPIRED
 }
